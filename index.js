324ed7d9e677835ff1ca2652d1b5573eda8bca51
Amritasingh16 <aamritassingh1@gmail.com>
2023-01-22 22:50:02 +0530

    RealtimeChatApp

---
+++ index.js
@@ -0,0 +1,27 @@
+const express= require("express")
+const app= express()
+const http= require('http').createServer(app)
+const io= require('socket.io')(http)
+
+const PORT= process.env.PORT||3000
+
+http.listen(PORT,()=>{
+    console.log(`App running on port ${PORT}`)
+})
+
+app.use(express.static(__dirname+'/public'))
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html')
+}
+)
+
+io.on('connection',(socket)=>{
+    console.log('Connected...')
+    socket.on('message',(msg)=>{
+       socket.broadcast.emit('message',msg)
+    })
+}
+)
+
